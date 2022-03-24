7b483bc3a8e6520bb75d84c573c6f269acce9ddf
Arathi485 <arathip8@gmail.com>
2022-03-24 18:01:45 +0530

    added

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+app_name='shop'
+
+
+urlpatterns = [
+    path('',views.allprodcat,name='allprodcat'),
+    path('<slug:c_slug>/',views.allprodcat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.prodDetail,name='prodCatdetail')
+]
